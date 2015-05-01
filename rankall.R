b5c8199e373610194676ebdda6fbb4d8594ef453
Robert Tuck <robtuck@gmail.com>
2015-05-01 00:52:26 -0400

    added reading from file and argument checks

--- rankall.R
+++ rankall.R
@@ -1,10 +1,23 @@
 rankall <- function(outcome, num = "best") {
-	## Read outcome data
-
-	## Check that state and outcome are valid
-
-	## For each state, find the hospital of the given rank
-
+  ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", 
+                   colClasses = "character")
+  
+  ## Check that state and outcome are valid
+  states <- unique(data[,7])
+  if(is.na(match(state, states))) {
+    stop("invalid state")
+  }
+  
+  validOutcomes <- c("heart attack" = 11, 
+                     "heart failure" = 17, 
+                     "pneumonia" = 23)
+  
+  if (is.na(match(outcome, names(validOutcomes)))) {
+    stop("invalid outcome")
+  }
+  
+  outcomeColumn <- validOutcomes[[outcome]]
 	## Return a data frame with the hospital names and the
 	## (abbreviated) state name
 }
